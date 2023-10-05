877e0943e40b574032b7f04f82fc14c59399a280
bingbong111 <bingbong1803@gmail.com>
2023-10-05 15:42:16 -0500

    shootable zombies with no anim

--- Zombie Game/Assets/Scripts/EnemyFollow.cs
+++ Zombie Game/Assets/Scripts/EnemyFollow.cs
@@ -6,16 +6,28 @@ public class EnemyFollow : MonoBehaviour
 {
     private Transform target;
     public float speed;
+    [SerializeField] float health, maxHealth = 3f;
 
     // Start is called before the first frame update
     void Start()
     {
         target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        health = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+    }
+
+    public void TakeDamage(float damageAmount)
+    {
+        health -= damageAmount;
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 }
