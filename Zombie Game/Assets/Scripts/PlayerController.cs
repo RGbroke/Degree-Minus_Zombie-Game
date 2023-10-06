c9f7f2612cf954f23a20d330c9cfb1f87ceb15f6
Xavier Enrique (Ricky) Gaviola <52550469+RGbroke@users.noreply.github.com>
2023-10-06 13:41:09 -0500

    Initial Health Bar Setup

--- Zombie Game/Assets/Scripts/PlayerController.cs
+++ Zombie Game/Assets/Scripts/PlayerController.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //Healthbar stuff
+    public int maxHealth = 50;
+    public int currHealth;
+    public HealthBar healthBar;
+
     public float moveSpeed = 5f;
     public Rigidbody2D rb;
 
     Vector2 moveDirection;
+
+    private void Start()
+    {
+        currHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
 
     // Update is called once per frame
     void Update()
@@ -16,10 +27,29 @@ public class PlayerController : MonoBehaviour
         float moveY = Input.GetAxisRaw("Vertical");
 
         moveDirection = new Vector2(moveX, moveY).normalized;
+
+        //Code to test healthbar. Remove later
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            this.TakeDamage(2);
+        }
     }
 
     private void FixedUpdate()
     {
         rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+    }
+
+    void TakeDamage(int damage)
+    {
+        if(currHealth-damage > 0)
+        {
+            currHealth -= damage;
+        }
+        else
+        {
+            currHealth = 0;
+        }
+        healthBar.SetHealth(currHealth);
     }
 }
