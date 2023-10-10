b3fb605a3d2e776f743516604bac34a07d728186
bingbong111 <bingbong1803@gmail.com>
2023-10-09 23:34:56 -0500

    added zombie animation

--- Zombie Game/Assets/Scripts/EnemyFollow.cs
+++ Zombie Game/Assets/Scripts/EnemyFollow.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class EnemyFollow : MonoBehaviour
 {
     private Transform target;
+    private float health;
     public float speed;
-    [SerializeField] float health, maxHealth = 3f;
+    public float maxHealth = 3f;
+    public Animator animator;
+    public Rigidbody2D zombie;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +24,9 @@ public class EnemyFollow : MonoBehaviour
     void Update()
     {
         transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+
+        animator.SetFloat("Horizontal", target.position.x - transform.position.x);
+        animator.SetFloat("Speed", speed);
     }
 
     public void TakeDamage(float damageAmount)
