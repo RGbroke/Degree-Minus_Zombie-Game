fae2f8d4f5fd7a49ad305b6787497d2d05b6a4f6
NGk71 <89110787+NGk71@users.noreply.github.com>
2023-10-31 15:40:15 -0500

    Revert "Recommit"
    This reverts commit 551875f0985b57969d62915a60401f1d248c5a3f.

--- Zombie Game/Assets/Scripts/Enemy Scripts/Enemy.cs
+++ Zombie Game/Assets/Scripts/Enemy Scripts/Enemy.cs
@@ -52,10 +52,9 @@ public class Enemy : MonoBehaviour
             new WaitForSeconds(1f);
         }
 
-       /* float distance = Vector3.Distance(this.transform.position, target.transform.position);
-        checkDistance();
-        if (gc.firstZombie == true)
-            resumeTimer();*/
+        float distance = Vector3.Distance(this.transform.position, target.transform.position);
+        if (distance <= 10 && gc.firstZombie != true)
+            seen();
     }
 
     public void TakeDamage(float damageAmount)
@@ -93,30 +92,12 @@ public class Enemy : MonoBehaviour
             playerComponent.TakeDamage(1);
         }
     }
-    /*
-    public void checkDistance()
-    {
-        float distance = Vector3.Distance(this.transform.position, target.transform.position);
-        if (distance <= 10 && gc.firstZombie != true)
-            seen();
-    }
-
-    public void resumeTimer()
-    {
-        if (Input.GetMouseButtonDown(1))
-        {
-            Time.timeScale = 1;
-            Debug.Log(gc.firstZombie);
-        }
-    }
+   
 
     private void seen()
     {
-        //gc.firstZombieSeen();
-	    //dialogue.index++;
-	    //Debug.Log(dialogue.index);
-        //Time.timeScale = 0;
+        gc.firstZombieSeen();
     }
-    */
+    
 
 }
--- Zombie Game/Assets/Scripts/Enemy Scripts/RangedEnemy.cs
+++ Zombie Game/Assets/Scripts/Enemy Scripts/RangedEnemy.cs
@@ -66,6 +66,7 @@ public class RangedEnemy : MonoBehaviour
 
     private void Start()
     {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
         rb = gameObject.GetComponent<Rigidbody2D>();
         timeToFire = 0f;
         health = maxHealth;
@@ -73,13 +74,8 @@ public class RangedEnemy : MonoBehaviour
 
     private void Update()
     {
-        if (!target)
-        {
-            GetTarget();
-        }
-        else
-        {
-            // Calculate the direction vector from the enemy to the player.
+        if (target != null)
+            {// Calculate the direction vector from the enemy to the player.
             Vector3 direction = (target.position - transform.position).normalized;
 
             // Determine if the player is on the left or right side of the zombie.
@@ -117,6 +113,10 @@ public class RangedEnemy : MonoBehaviour
         {
             Shoot();
         }
+
+        float distance = Vector3.Distance(this.transform.position, target.transform.position);
+        if (distance <= 15 && gc.secondZombie != true)
+            seen();
     }
 
     private void RotateFiringPoint()
@@ -163,13 +163,7 @@ public class RangedEnemy : MonoBehaviour
         }
     }
 
-    private void GetTarget()
-    {
-        if (GameObject.FindGameObjectWithTag("Player"))
-        {
-            target = GameObject.FindGameObjectWithTag("Player").transform;
-        }
-    }
+   
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -185,5 +179,10 @@ public class RangedEnemy : MonoBehaviour
         {
             playerComponent.TakeDamage(1);
         }
+    }
+
+    private void seen()
+    {
+        gc.secondZombieSeen();
     }
 }
