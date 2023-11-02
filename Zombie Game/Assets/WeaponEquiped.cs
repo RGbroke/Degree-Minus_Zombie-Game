ff86327e189a657668364ee11b90176be494fcb9
Xavier Enrique (Ricky) Gaviola <52550469+RGbroke@users.noreply.github.com>
2023-11-02 00:01:17 -0500

    Weapon UI Framework
    Updated the Canvas prefab to incorporate the UI updates discussed. Has a framework for weapon switching between 3 weapons. Can probably be updated to handle more/less. New UI can be previewed in the UI_Testing Scene located in the Assets/Scenes/Testers/UI_Testing

---
+++ Zombie Game/Assets/WeaponEquiped.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class WeaponEquiped : MonoBehaviour
+{
+    [SerializeField]
+    GameObject weapon1;
+
+    [SerializeField]
+    GameObject weapon2;
+
+    [SerializeField]
+    GameObject weapon3;
+
+    [SerializeField]
+    Animator switchVisual;
+
+    private int equiped = 0;
+
+    public void nextWeapon(InputAction.CallbackContext context)
+    {
+        if (!context.started)
+            return;
+
+        nextWeapon();
+    }
+    public void nextWeapon()
+    {
+        switchVisual.SetTrigger("Next");
+        equiped = (equiped + 1) % 3;
+        UnityEngine.Debug.Log("Weapon: " + equiped);
+    }
+
+    public void prevWeapon(InputAction.CallbackContext context)
+    {
+        if (!context.started)
+            return;
+
+        switchVisual.SetTrigger("Prev");
+        equiped = (equiped - 1) % 3;
+        UnityEngine.Debug.Log("Weapon: " + equiped);
+    }
+    public void prevWeapon()
+    {
+        switchVisual.SetTrigger("Prev");
+        equiped = (equiped-1) % 3;
+        UnityEngine.Debug.Log("Weapon: " + equiped);
+    }
+
+    public GameObject getEquiped(InputAction.CallbackContext context)
+    {
+        switch (equiped)
+        {
+            case 0:
+                return weapon1;
+            case 1:
+                return weapon2;
+            case 2:
+                return weapon3;
+        }
+        return null; //Should never run but just in case
+    }
+}
